b5192ee16ebe7859048bf2ea9cf7840e5be263ac
Kunal Poddar <kunal.poddar@codepoetry.in>
2022-09-08 09:17:43 +0530

    Testing dependencies and boilerplate is added using chai & mocha

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   return res.send('Hello this get request')
 })
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Server running at port 3000')
 })
+
+module.exports = app
