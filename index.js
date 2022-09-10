6a6b5670ab9f96629da98adf0ca4283e728dbff7
Kunal Poddar <kunal.poddar@codepoetry.in>
2022-09-10 07:13:26 +0530

    ejs, View added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
 const app = express()
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  return res.send('Hello this get request')
+  return res.render('index')
 })
 
 app.listen(3000, () => {
